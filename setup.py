12f2619712053abbce5bd4555155702ac4b0d716
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2020-04-27 17:13:16 +0200

    update

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name='cdiscrap',
-    version='2.0',
-    license='MIT',
-    author='pierredarrieutort, mathieudaix',
-    author_email='p.darrieutort@outlook.fr, mathieudaixpro@gmail.com',
-    description='Scrap Cdiscount to get price of product from its productID'
+    name="cdiscrap",
+    version="0.0.3",
+    author="Mathieu Daix, Pierre Darrieutort",
+    author_email="mathieudaixpro@gmail.com, p.darrieutort@outlook.com",
+    description="Python package capable of raising the price of any product on the site www.cdiscount.com",
+    url="https://github.com/pierredarrieutort/cdiscrap",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8.2',
 )
