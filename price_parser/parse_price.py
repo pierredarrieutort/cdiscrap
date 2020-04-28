f8e0802745e7653959690be4b2b563e32cc36d10
Pierre DARRIEUTORT <p.darrieutort@outlook.fr>
2020-04-28 15:54:47 +0200

    update setup

--- price_parser/parse_price.py
+++ price_parser/parse_price.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 import urllib.request
-import urllib.parse
 import re
 
 
--- setup.py
+++ setup.py
@@ -14,6 +14,11 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
+    install_requires=[
+        'beautifulsoup4',
+        'urllib.request',
+        're'
+    ],
     extras_require = {
         "dev": [
             "pytest>=3.7"
